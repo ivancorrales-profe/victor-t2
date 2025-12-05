b67bc71efd4556ce5397a1fc882fecb4e12084dd
victoralejandromorenoquevedo-glitch <victoralejandromorenoquevedo@gmail.com>
2025-12-05 14:17:57 +0100

    guardar

---
+++ src/main/java/com/victoralejandro/poo/redsocial/redsocial.java
@@ -0,0 +1,18 @@
+package com.victoralejandro.poo.redsocial;
+import java.time.LocalTime;
+public class redsocial {
+	private String texto;
+	private LocalTime hora;
+	private int numeroLikes;
+	public void mostrar () {
+		this.texto = texto;
+		this.hora = hora;
+		this.numeroLikes = numeroLikes;
+		System.out.println(texto);
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
